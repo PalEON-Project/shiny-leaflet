a4ba6184f9d7923102eda550fdf26965f1877211
Simon <simon.j.goring@gmail.com>
2015-11-24 14:38:53 -0800

    Cleaning files slowly. . . Some of these seem to be taking a long time to stage, so I'm doing this in chunks.

--- ExtraFiles/CanadaShapefile/ui.R
+++
@@ -1,5 +0,0 @@
-shinyUI(pageWithSidebar(
-  headerPanel("shapefileTest"),
-  sidebarPanel(
-    selectInput("mapColor","choose color of Canada:",choices=c("red","blue","yellow"))),
-                mainPanel(plotOutput("MapPlot",height="800px"))))
