5221bf425ecc05c349e3cdb85d55e7bf7ffa5a52
Simon <simon.j.goring@gmail.com>
2015-12-30 14:45:56 -0800

    Cleaning up changes associated with the removal of shapefiles &cetera

--- ExtraFiles/Modified Shapefile/ui.R
+++
@@ -1,5 +0,0 @@
-shinyUI(pageWithSidebar(
-  headerPanel("shapefileTest"),
-  sidebarPanel(
-    selectInput("mapColor","choose color of USA:",choices=c("gray","blue","yellow"))),
-                mainPanel(plotOutput("MapPlot",height="800px"))))
